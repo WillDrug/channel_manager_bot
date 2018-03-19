4e8f0f5c189218925b35563c4e99605df5b63cbc
willdrug <whydafuqdoineedthis@gmail.com>
2018-03-19 14:14:37 +0300

    Initial

--- bot.py
+++ bot.py
@@ -1,18 +1,24 @@
 from flask import Flask, request
 import telepot
 import urllib3
+import os
+from uuid import uuid4
 
+# ----Pythonanywhere specific part--------
 proxy_url = "http://proxy.server:3128"
 telepot.api._pools = {
     'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
 }
-telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+telepot.api._onetime_pool_spec = (
+urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+# ----------------------------------------
 
-secret = "A_SECRET_NUMBER"
+secret = uuid4()
 bot = telepot.Bot('447645569:AAGt21UW3fuydM4TCEPV2StZWl71tB5tCII')
 bot.setWebhook("https://willdrug.pythonanywhere.com/{}".format(secret), max_connections=1)
 
 app = Flask(__name__)
+
 
 @app.route('/{}'.format(secret), methods=["POST"])
 def telegram_webhook():
@@ -20,5 +26,5 @@ def telegram_webhook():
     if "message" in update:
         text = update["message"]["text"]
         chat_id = update["message"]["chat"]["id"]
-        bot.sendMessage(chat_id, "From the web: you said '{}'".format(text))
+        bot.sendMessage(chat_id, f"{os.environ.get('SECRET')}")
     return "OK"
