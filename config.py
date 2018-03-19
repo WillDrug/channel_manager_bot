111e58703a3891f60717868c32b1365c6cf05630
willdrug <whydafuqdoineedthis@gmail.com>
2018-03-19 23:35:55 +0300

    testing mysql

--- config.py
+++ config.py
@@ -9,8 +9,6 @@ path = uuid4()
 class Production:
     @staticmethod
     def init_proxy():
-        project_folder = os.path.expanduser('~/channel_manager_bot')  # adjust as appropriate
-        load_dotenv(os.path.join(project_folder, '.env'))
         # ----Pythonanywhere specific part--------
         proxy_url = "http://proxy.server:3128"
         telepot.api._pools = {
@@ -19,19 +17,23 @@ class Production:
         telepot.api._onetime_pool_spec = (
         urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
         # ----------------------------------------
+    def __init__(self):
+        project_folder = os.path.expanduser('~/channel_manager_bot')  # adjust as appropriate
+        load_dotenv(os.path.join(project_folder, '.env'))
 
-    db_conn = "mysql://willdrug:%s@willdrug.mysql.pythonanywhere-services.com/willdrug$cmb" % os.environ.get('SQLPWD')
-    print(db_conn)
-    webhook_addr = f"https://willdrug.pythonanywhere.com/{path}"
+        db_conn = "mysql://willdrug:%s@willdrug.mysql.pythonanywhere-services.com/willdrug$cmb" % os.environ.get('SQLPWD')
+        webhook_addr = f"https://willdrug.pythonanywhere.com/{path}"
 
 class Test:
     @staticmethod
     def init_proxy():
-        project_folder = os.path.expanduser('F:\\WOrk\\channel_manager_bot')  # adjust as appropriate
+        pass
+
+    def __init__(self):
+        project_folder = os.path.expanduser('F:\\Work\\channel_manager_bot')  # adjust as appropriate
         load_dotenv(os.path.join(project_folder, '.env'))
+        db_conn = "sqlite:///test.db"
+        webhook_addr = ''
 
-    db_conn = "sqlite:///test.db"
-    webhook_addr = ''
 
-config = Production
-config.init_proxy()
+config = Production()
