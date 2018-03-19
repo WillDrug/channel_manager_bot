8c9f942529d69a67db7ec4bccbc3e04168fe088f
willdrug <whydafuqdoineedthis@gmail.com>
2018-03-19 14:02:11 +0300

    Initial

---
+++ bot.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+import telepot
+import urllib3
+
+proxy_url = "http://proxy.server:3128"
+telepot.api._pools = {
+    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+}
+telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+
+secret = "A_SECRET_NUMBER"
+bot = telepot.Bot('447645569:AAGt21UW3fuydM4TCEPV2StZWl71tB5tCII')
+bot.setWebhook("https://willdrug.pythonanywhere.com/{}".format(secret), max_connections=1)
+
+app = Flask(__name__)
+
+@app.route('/{}'.format(secret), methods=["POST"])
+def telegram_webhook():
+    update = request.get_json()
+    if "message" in update:
+        text = update["message"]["text"]
+        chat_id = update["message"]["chat"]["id"]
+        bot.sendMessage(chat_id, "From the web: you said '{}'".format(text))
+    return "OK"
