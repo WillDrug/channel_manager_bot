c01eadadf6d5b93a6526237e8db84c42e35ee61c
WillDrug <j3qq4h7h2v>
2018-03-21 10:14:46 +0300

    1

--- config.py
+++ config.py
@@ -6,24 +6,24 @@ from uuid import uuid4
 path = uuid4()
 
 
-class Production:
-    @staticmethod
-    def init_proxy():
-        # ----Pythonanywhere specific part--------
-        proxy_url = "http://proxy.server:3128"
-        telepot.api._pools = {
-            'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
-        }
-        telepot.api._onetime_pool_spec = (
+def init_proxy():
+    # ----Pythonanywhere specific part--------
+    proxy_url = "http://proxy.server:3128"
+    telepot.api._pools = {
+        'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+    }
+    telepot.api._onetime_pool_spec = (
         urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
-        # ----------------------------------------
+    # ----------------------------------------
+
+class Production:
     def __init__(self):
         project_folder = os.path.expanduser('~/channel_manager_bot')  # adjust as appropriate
         load_dotenv(os.path.join(project_folder, '.env'))
 
         self.db_conn = "mysql://willdrug:%s@willdrug.mysql.pythonanywhere-services.com/willdrug$cmb" % os.environ.get('SQLPWD')
         self.webhook_addr = f"https://willdrug.pythonanywhere.com/{path}"
-        Production.init_proxy()
+        init_proxy()
 
 class Test:
     def __init__(self):
