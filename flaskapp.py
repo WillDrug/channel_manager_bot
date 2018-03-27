0e4406d8b160dfa1159268f554c5af40b4615987
willdrug <j3qq4h7h2v>
2018-03-27 11:48:07 +0300

    logging

--- flaskapp.py
+++ flaskapp.py
@@ -1,7 +1,7 @@
 from config import config
 from bot import bot, handle
 from flask import Flask, request
-
+import logging
 
 app = Flask(__name__)
 
@@ -10,6 +10,8 @@ bot.setWebhook(config.webhook_addr, max_connections=1)
 @app.route(f'/{config.path}', methods=["POST"])
 def telegram_webhook():
     update = request.get_json()
-    if 'message' in update.keys():
+    if 'message' in update.keys() or 'channel_post' in update.keys():
         handle(update['message'])
+    else:
+        logging.critical(f'Weird message {update} received')
     return "OK"
